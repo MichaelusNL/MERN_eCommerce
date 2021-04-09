6c892cf01be7ec986aa2f03c6ad8edf10d209478
mike <mikeruigrok@live.nl>
2021-04-09 16:14:37 +1200

    Prepare for Heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -38,11 +38,6 @@ app.use(express.json())
 
 connectDB()
 
-// Test API index
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
-
 // The /api/products route logic
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -52,10 +47,20 @@ app.use('/api/upload', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
-
 const __dirname = path.resolve()
-
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  // Test API index
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 
